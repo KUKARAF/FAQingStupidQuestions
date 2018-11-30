69ce08a3ed1c1813a68e1f1e92cc75dfcfdac7c4
Burning_Wipf <Rafal.kuka94@gmail.com>
2018-12-01 00:57:02 +0100

    Elasticlunr + fuzzySet
    Abandoned the idea of a manual brute forcce search and integrated Elasticlunr and fuzzyset as alternative for correction in case of 0 results. 
    
    Fuzzyset threshhold need to be adjusted.

--- search.js
+++ search.js
@@ -1,83 +1,13 @@
+//elasticlunr init
+var index = elasticlunr(function () {
+    this.addField('title');
+    this.addField('body');
+    this.setRef('id');
+});
 
 
 
-
-
-function repeat(fn, times) {
-  var loop = function (times) {
-    if (times) {
-      fn(times);
-      loop(--times);
-    }
-  }
-  loop(times);
-}
-
-
-repeat(function(){document.getElementsByTagName('h2')[0].remove()},9)
-repeat(function(){document.getElementsByTagName('h3')[0].remove()},61)
-//ignore this, im just to lazy to properly fix the html
-
-
-
-
-
-// creates object from a json file
-// key:{'titleOrNum':[lis,of,paragrafs],[list,of,tags],'HTMLtable'}
-function CrtElemt(key) {
-  wrapper = document.createElement('li');
-  title = document.createElement("div");
-  desc = document.createElement("div");
-//pass
-
-}
-
-
-//document.getElementById('comboBoxData').getElementsByTagName('li')
-
-function isIterable(obj) {
-  // checks for null and undefined
-  if (obj == null) {
-    return false;
-  }
-  return typeof obj[Symbol.iterator] === 'function';
-}
-
-
-function ShowResult(result, hb = true) {//hides or shows list or individual results
-  if(isIterable(result)){
-    c = result.length
-      for(var i=0; i<c; i++){
-          ShowResult(result[i],hb)
-      }
-  }else{
-  if(hb){
-  result.style.height = 'auto';
-  result.style.visibility = 'visible'
-}else {
-  result.style.height = 0;
-  result.style.visibility = 'hidden'
-}}
-}
-
-ShowResult(document.getElementById('comboBoxData').getElementsByTagName('li'),false); //hides all li items in combobox
-
-
-
-
-
-function searchSimple(q, tlc=true) { //returns found html tags
-  var testAgainst = document.getElementById('comboBoxData').getElementsByTagName('li'); //needs to be adjusted to search json
-  c = testAgainst.length;
-  var r =[];
-  for(var i =0; i<c; i++){
-    if( testAgainst[i].textContent.toLowerCase().includes(q.toLowerCase())){
-    r.push(testAgainst[i])
-    }
-  }
-  return r
-}
-
+//Fuzzy set data
 
 dataSet = document.getElementById('comboBoxData').textContent.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(' ');
 function findCloseWords(q, t=0.7, tlc=true) {
@@ -92,10 +22,83 @@ r.forEach(function(element) {
 return res
 }
 
-function suggest(original,instead) { //will be used to suggest corrected results
-  document.getElementById('showInstead').value = 'Showin Resulsts For:' + ' ' + instead
 
+//indexing of FAQ
+/*
+var doc1 = {
+    "id": 1,
+    "title": "Oracle released its latest database Oracle 12g",
+    "body": "Yesterday Oracle has released its new database Oracle 12g, this would make more money for this company and lead to a nice profit report of annual year."
 }
+
+var doc2 = {
+    "id": 2,
+    "title": "Oracle released its profit report of 2015",
+    "body": "As expected, Oracle released its profit report of 2015, during the good sales of database and hardware, Oracle's profit of 2015 reached 12.5 Billion."
+}
+
+
+index.addDoc(doc1);
+index.addDoc(doc2);
+*/
+
+//console.log(index.search("Oracle database profit"));
+
+
+
+
+
+function repeat(fn, times) {  //repeats a function x times, cause im lazy
+  var loop = function (times) {
+    if (times) {
+      fn(times);
+      loop(--times);
+    }
+  }
+  loop(times);
+}
+
+
+
+
+// gen data set for indexing
+{
+  allFaq = document.getElementsByClassName('faq-list')
+  ac = allFaq.length
+  for(var i = 0; ac > i; i++){ //for Item in faq-list
+    subLisAll = allFaq[i].childNodes;
+    sC = subLisAll.length
+    for(var is = 0;sC>is; is++){ // for li in faq list
+      if(subLisAll[is].tagName == 'LI'){
+        docnum = 'ID'+ i.toString() +'--'+ is.toString()
+         //body
+        var doc = {
+            "id": docnum,
+            "title": subLisAll[is].getElementsByTagName('a')[0].innerText,
+            "body": subLisAll[is].getElementsByTagName('ul')[0].innerText
+        }
+        index.addDoc(doc);
+
+      }
+    }
+
+  }
+}
+//document.getElementById('comboBoxData').getElementsByTagName('li')
+
+function isIterable(obj) { // true falls if iterable
+  // checks for null and undefined
+  if (obj == null) {
+    return false;
+  }
+  return typeof obj[Symbol.iterator] === 'function';
+}
+
+
+
+
+
+
 
 function addTextAreaCallback(textArea, callback, delay) {
     var timer = null;
@@ -109,22 +112,23 @@ function addTextAreaCallback(textArea, callback, delay) {
         }, delay );
     };
     textArea = null;
-}//not used yet
+}//not used yet might be usefull if search function is too memory intensive
+
+
 
 
 document.getElementById("comboBox").addEventListener("keydown", function() {
-ShowResult(document.getElementById('comboBoxData').getElementsByTagName('li'),false) //hides everything
-if(this.value != ''){
-close = findCloseWords(this.value)[0]
-found = searchSimple(this.value)
-if(found.length){
-  ShowResult(found)
-  console.log('non fuzzy, found: '+ found.length+' results' )
-}else if(close != null){
-  console.log('corrected to: '+close)
-  ShowResult(searchSimple(close))
-}else{
-  console.log('Nothing found for: '+this.value)
+  results = index.search(this.value) //searches for value returns object
+  console.log(this.value)
+  console.log(results)
+  if(!results.length){ //if no results try fuzzy search
+    close = findCloseWords(this.value)[0]
+    results = index.search(close)
+    console.log('Corrected to:  '+ close)
+  }
+  document.getElementById('tit').innerText = results[0].doc.title;
+  document.getElementById('bodey').innerText = results[0].doc.body;
+
+
 }
-}}
 )
